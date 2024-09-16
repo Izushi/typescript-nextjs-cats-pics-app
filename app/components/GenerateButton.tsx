6d924711e010ff88d53c62f297f72c008edecd8e
Izushi <shoeshinesamurai@gmail.com>
2024-09-15 21:15:35 -0700

    set type to API respons with TypeScript

--- app/components/GenerateButton.tsx
+++ app/components/GenerateButton.tsx
@@ -1,14 +1,27 @@
 'use client'
 import styles from '../../styles/Home.module.css'
 
+interface SearchCatImageResponse {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
 export default function GenerateButton() {
-  const fetchCatImage = async () => {
+  const fetchCatImage = async (): Promise<SearchCatImageResponse> => {
     const res = await fetch("https://api.thecatapi.com/v1/images/search");
     const data = await res.json();
-    console.log(data);
+    return data[0];
   }
+
+  const handleClick = async () => {
+    const catImage = await fetchCatImage();
+    console.log(catImage.url);
+  }
+
   return (
-    <button className={styles.button} onClick={fetchCatImage}>
+    <button className={styles.button} onClick={handleClick}>
       Generate
     </button>
   );
